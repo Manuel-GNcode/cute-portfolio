1e46c0d68082724f40e2db57c0925d3cd2242fda
GNcode <gngalarga027@gmail.com>
2024-06-27 12:50:03 -0500

    Creando animaciones de la portada de la página.

--- front/front.jsx
+++ front/front.jsx
@@ -1,14 +1,25 @@
 import './front.css';
 
 export const Front = ()=>{
+    const hideFront = ()=>{
+        const front = document.querySelector('#cuteFront');
+        const mark = document.querySelector('.cuteFront-mark');
+        const avatar = document.querySelector('.cuteFront-avatar');
+        front.style.animation = 'hideFront .4s ease-out .5s forwards';
+        mark.style.animation = 'hideMark .4s cubic-bezier(1.000, -0.460, 1.000, 1.015) forwards';
+        avatar.style.animation = 'hideAvatar .4s cubic-bezier(1.000, -0.460, 1.000, 1.015) forwards';
+    }
+
     return (
         <article id="cuteFront">
-            <section className="cuteFront-plate">
-                <img src="front/public/cuteFront-logo.png" alt="logo" />
-                <span>START</span>
+            <section className="cuteFront-mark">
+                <div className='cuteFront-mark-plate'>
+                    <img className='cuteFront-mark-logo' src="" alt="Logo" />
+                </div>
+                <button onClick={hideFront} type='button' className='cuteFront-mark-btn'>START</button>
             </section>
-            <div className="cuteFront-avatar">
-            </div>
+
+            <img className='cuteFront-avatar' src="" alt="Avatar" />
         </article>
     )
 }
